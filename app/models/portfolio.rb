c854a76bda691eceaad4bf21d064709b91af689c
Sunny <sunnyinvadeher@gmail.com>
2018-05-16 00:51:17 +0800

    Implememted concern for managin data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+	include Placeholder
 	validates :title, :body, :main_image, :thumb_image, presence: true
 
 	scope :angular, -> { where(subtitle: 'Angular') }
@@ -7,9 +8,7 @@ class Portfolio < ApplicationRecord
 	after_initialize :set_defaults
 
 	def set_defaults
-		self.main_image ||= "http://via.placeholder.com/600x400"
-		self.thumb_image ||= "http://via.placeholder.com/350x200"
+		self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+		self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
 	end
-
-
 end
