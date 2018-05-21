1565addb32c52a16c37afa0520e33752fe1b0abb
Sunny <sunnyinvadeher@gmail.com>
2018-05-21 21:16:35 +0800

    Practiced some helper

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   
   get "about", to: "pages#about"
-  get "contact", to: "pages#contant"
+  get "contact", to: "pages#contact"
 
   resources :portfolios
   get 'angular-items', to: "portfolios#angular"
